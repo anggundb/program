9cafa287817369375907d4514f5abc80c84cc3c6
anggundb <57027426+anggundb@users.noreply.github.com>
2021-12-08 19:59:26 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,17 +1,6 @@
-<?php
- 
-// Konfigurasi Database
-$host       = 'belajar'; // host
-$username   = 'root'; // username database
-$password   = '123'; // password database
-$dbname     = 'koneksi'; // nama database
- 
-$db = mysqli_connect($host, $username, $password, $dbname);
- 
-if ($db) {
-    echo "Database Terhubung";
-} else {
-    echo "Database Error";
+<?php 
+if (!mysqli_connect('localhost', 'anggun', '123', 'koneksi')) { 
+die('Koneksi gagal: ' . mysqli_connect_error()); 
 }
-
+echo 'Koneksi berhasil';
 ?>
